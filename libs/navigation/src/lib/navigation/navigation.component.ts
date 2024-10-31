f8931bf5447bd7ed104a8816bb56646ddbfd5a74
maciejkoch <maciejkoch90@gmail.com>
2024-10-31 12:46:55 +0100

    mobile navigation

--- libs/navigation/src/lib/navigation/navigation.component.ts
+++ libs/navigation/src/lib/navigation/navigation.component.ts
@@ -1,4 +1,4 @@
-import { Component, output } from '@angular/core';
+import { Component, output, signal } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
@@ -8,9 +8,25 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   templateUrl: './navigation.component.html',
 })
 export class NavigationComponent {
+  desktop = signal(true);
+
   items = [
     { link: '', label: 'Blog', directMatch: true },
-    { link: '/calculators', label: 'Kalkulatory' },
+    {
+      link: '/calculators',
+      label: 'Kalkulatory',
+      open: signal(false),
+      items: [
+        {
+          link: '/calculators/long-term-savings',
+          label: 'Cel długoterminowy',
+        },
+        {
+          link: '/calculators/regular-contribution',
+          label: 'Regularne oszczędzanie',
+        },
+      ],
+    },
     { link: '/about-us', label: 'O nas' },
   ];
 
