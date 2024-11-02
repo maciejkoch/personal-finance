60730f1cd5a91d8edc9674b1c7393b218bee13a4
maciejkoch <maciejkoch90@gmail.com>
2024-11-02 12:04:16 +0100

    eslint config for functions

--- functions/eslint.config.js
+++ functions/eslint.config.js
@@ -4,10 +4,21 @@ module.exports = [
   },
   {
     files: ['**/*.ts'],
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint', 'import'],
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:import/errors',
+      'plugin:import/warnings',
+      'plugin:import/typescript',
+    ],
     rules: {
       quotes: ['error', 'double'],
       'import/no-unresolved': 0,
       indent: ['error', 2],
+      '@typescript-eslint/no-unused-vars': ['error'],
+      '@typescript-eslint/explicit-function-return-type': 'off',
     },
   },
 ];
