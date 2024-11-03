4902bd51b27e7406406249e5090c04fcc39de002
maciejkoch <maciejkoch90@gmail.com>
2024-11-03 15:05:41 +0100

    hero component enhancements

--- libs/ui/src/lib/hero.component.ts
+++ libs/ui/src/lib/hero.component.ts
@@ -3,6 +3,7 @@ import {
   Component,
   input,
   output,
+  signal,
 } from '@angular/core';
 
 @Component({
@@ -13,9 +14,27 @@ import {
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HeroComponent {
+  ANIMATION_DURATION = 300;
+
   title = input<string>();
   text = input<string>();
   buttonLabel = input<string>();
+  expandable = input(true);
 
   clicked = output<void>();
+
+  expanded = signal(true);
+
+  toggle(value?: boolean) {
+    if (!this.expandable()) {
+      return;
+    }
+
+    this.expanded.update((expanded) => value || !expanded);
+  }
+
+  handleClick() {
+    this.toggle(false);
+    setTimeout(() => this.clicked.emit(), this.ANIMATION_DURATION);
+  }
 }
