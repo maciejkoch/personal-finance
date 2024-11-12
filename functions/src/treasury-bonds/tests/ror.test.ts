1079c9a64a4dc5830da11a9b68bfca2ccda46680
maciejkoch <maciejkoch90@gmail.com>
2024-11-12 12:33:51 +0100

    fixed ror

--- functions/src/treasury-bonds/tests/ror.test.ts
+++ functions/src/treasury-bonds/tests/ror.test.ts
@@ -16,6 +16,7 @@ describe('Calculate treasury bonds', () => {
       savingsRate: [0.06],
     };
     const result = calculate(ROR, 100, 144, calculationParams);
+
     const expectedResult1: TreasuryBondsMonthlyResult = {
       rate: 0.0575,
       numberOfBonds: 100,
@@ -37,165 +38,71 @@ describe('Calculate treasury bonds', () => {
     };
     deepStrictEqual(result[1], expectedResult1);
 
-    // const expectedResult14: TreasuryBondsMonthlyResult = {
-    //   rate: 0.04,
-    //   numberOfBonds: 100,
-    //   nextNumberOfBonds: 100,
-    //   buyCost: 9990,
-    //   nextBuyCost: 9990,
-    //   nominalValue: 10000,
-    //   nextNominalValue: 10000,
-    //   basisOfCapitalisation: 10000,
-    //   nextBasisOfCapitalisation: 10000,
-    //   maturity: false,
-    //   valueAtEndOfMonth: 10033.33,
-    //   earlyBuyoutPenalty: 50,
-    //   buyoutAtEndOfMonth: 9986.5,
-
-    //   profitAtEndOfMonth: 27,
-    //   balanceAtEndOfMonth: 411.96,
-    //   finalValueAtEndOfMath: 10371.46,
-    // };
-    // deepStrictEqual(result[14], expectedResult14);
-
-    // const expectedResult24: TreasuryBondsMonthlyResult = {
-    //   rate: 0.04,
-    //   numberOfBonds: 100,
-    //   nextNumberOfBonds: 100,
-    //   buyCost: 9990,
-    //   nextBuyCost: 9990,
-    //   nominalValue: 10000,
-    //   nextNominalValue: 10000,
-    //   basisOfCapitalisation: 10000,
-    //   nextBasisOfCapitalisation: 10000,
-    //   maturity: true,
-    //   valueAtEndOfMonth: 10033.33,
-    //   earlyBuyoutPenalty: 0,
-    //   buyoutAtEndOfMonth: 10027,
-
-    //   profitAtEndOfMonth: 37,
-    //   balanceAtEndOfMonth: 706.52,
-    //   finalValueAtEndOfMath: 10696.52,
-    // };
-    // deepStrictEqual(result[24], expectedResult24);
-  });
-
-  xit('should work for ROR with 10000 bonds', () => {
-    const calculationParams: CalculationParams = {
-      inflation: [0.1],
-      referenceRate: [0.04],
-      wibor6m: [0.04],
-      savingsRate: [0.04],
-    };
-    const result = calculate(ROR, 10000, 50, calculationParams);
-
-    const expectedResult1: TreasuryBondsMonthlyResult = {
-      rate: 0.0525,
-      numberOfBonds: 10000,
-      nextNumberOfBonds: 10000,
-      buyCost: 1000000,
-      nextBuyCost: 1000000,
-      nominalValue: 1000000,
-      nextNominalValue: 1000000,
-      basisOfCapitalisation: 1000000,
-      nextBasisOfCapitalisation: 1000000,
-      maturity: false,
-      valueAtEndOfMonth: 1004375,
-      earlyBuyoutPenalty: 4375,
-      buyoutAtEndOfMonth: 1000000,
-
-      profitAtEndOfMonth: 3543.75,
-      balanceAtEndOfMonth: 3543.75,
-      finalValueAtEndOfMath: 1000000,
-    };
-    deepStrictEqual(result[1], expectedResult1);
-
-    const expectedResult2: TreasuryBondsMonthlyResult = {
-      rate: 0.04,
-      numberOfBonds: 10000,
-      nextNumberOfBonds: 10000,
-      buyCost: 1000000,
-      nextBuyCost: 1000000,
-      nominalValue: 1000000,
-      nextNominalValue: 1000000,
-      basisOfCapitalisation: 1000000,
-      nextBasisOfCapitalisation: 1000000,
-      maturity: false,
-      valueAtEndOfMonth: 1003333.33,
-      earlyBuyoutPenalty: 5000,
-      buyoutAtEndOfMonth: 998650,
-
-      profitAtEndOfMonth: 2700,
-      balanceAtEndOfMonth: 6253.32,
-      finalValueAtEndOfMath: 1002203.32,
-    };
-    deepStrictEqual(result[2], expectedResult2);
-
     const expectedResult12: TreasuryBondsMonthlyResult = {
-      rate: 0.04,
-      numberOfBonds: 10000,
-      nextNumberOfBonds: 10037,
-      buyCost: 1000000,
-      nextBuyCost: 1002696.3,
-      nominalValue: 1000000,
-      nextNominalValue: 1003700,
-      basisOfCapitalisation: 1000000,
-      nextBasisOfCapitalisation: 1003700,
+      rate: 0.0575,
+      numberOfBonds: 100,
+      nextNumberOfBonds: 104,
+      buyCost: 10000,
+      nextBuyCost: 10390,
+      nominalValue: 10000,
+      nextNominalValue: 10400,
+      basisOfCapitalisation: 10000,
+      nextBasisOfCapitalisation: 10400,
       maturity: true,
-      valueAtEndOfMonth: 1003333.33,
+      valueAtEndOfMonth: 10047.92,
       earlyBuyoutPenalty: 0,
-      buyoutAtEndOfMonth: 1002700.0,
+      buyoutAtEndOfMonth: 10038.81,
 
-      profitAtEndOfMonth: 3.7,
-      balanceAtEndOfMonth: 31058.35,
-      finalValueAtEndOfMath: 1033754.65,
+      profitAtEndOfMonth: 48.81,
+      balanceAtEndOfMonth: 486.27,
+      finalValueAtEndOfMath: 10476.27,
     };
     deepStrictEqual(result[12], expectedResult12);
 
-    const expectedResult13: TreasuryBondsMonthlyResult = {
-      rate: 0.0525,
-      numberOfBonds: 10037,
-      nextNumberOfBonds: 10037,
-      buyCost: 1002696.3,
-      nextBuyCost: 1002696.3,
-      nominalValue: 1003700,
-      nextNominalValue: 1003700,
-      basisOfCapitalisation: 1003700,
-      nextBasisOfCapitalisation: 1003700,
+    const expectedResult14: TreasuryBondsMonthlyResult = {
+      rate: 0.0575,
+      numberOfBonds: 104,
+      nextNumberOfBonds: 104,
+      buyCost: 10390,
+      nextBuyCost: 10390,
+      nominalValue: 10400,
+      nextNominalValue: 10400,
+      basisOfCapitalisation: 10400,
+      nextBasisOfCapitalisation: 10400,
       maturity: false,
-      valueAtEndOfMonth: 1008091.19,
-      earlyBuyoutPenalty: 4391.19,
-      buyoutAtEndOfMonth: 1003700.0,
+      valueAtEndOfMonth: 10449.83,
+      earlyBuyoutPenalty: 52,
+      buyoutAtEndOfMonth: 10398.25,
 
-      profitAtEndOfMonth: 3556.86,
-      balanceAtEndOfMonth: 34699.07,
-      finalValueAtEndOfMath: 1034842.2,
+      profitAtEndOfMonth: 40.37,
+      balanceAtEndOfMonth: 167.86,
+      finalValueAtEndOfMath: 10525.74,
     };
-    deepStrictEqual(result[13], expectedResult13);
+    deepStrictEqual(result[14], expectedResult14);
 
-    const expectedResult50: TreasuryBondsMonthlyResult = {
-      rate: 0.04,
-      numberOfBonds: 10148,
-      nextNumberOfBonds: 10148,
-      buyCost: 1013785.2,
-      nextBuyCost: 1013785.2,
-      nominalValue: 1014800,
-      nextNominalValue: 1014800,
-      basisOfCapitalisation: 1014800,
-      nextBasisOfCapitalisation: 1014800,
-      maturity: false,
-      valueAtEndOfMonth: 1018182.67,
-      earlyBuyoutPenalty: 5074,
-      buyoutAtEndOfMonth: 1013430.02,
+    const expectedResult144: TreasuryBondsMonthlyResult = {
+      rate: 0.0575,
+      numberOfBonds: 168,
+      nextNumberOfBonds: 175,
+      buyCost: 16784,
+      nextBuyCost: 17483.2,
+      nominalValue: 16800,
+      nextNominalValue: 17500,
+      basisOfCapitalisation: 16800,
+      nextBasisOfCapitalisation: 17500,
+      maturity: true,
+      valueAtEndOfMonth: 16880.5,
+      earlyBuyoutPenalty: 0,
+      buyoutAtEndOfMonth: 16865.21,
 
-      profitAtEndOfMonth: 2739.96,
-      balanceAtEndOfMonth: 138341.44,
-      finalValueAtEndOfMath: 1149031.5,
+      profitAtEndOfMonth: 0,
+      balanceAtEndOfMonth: 797.71,
+      finalValueAtEndOfMath: 17662.91,
     };
-    deepStrictEqual(result[50], expectedResult50);
+    deepStrictEqual(result[144], expectedResult144);
   });
 
-  xit('should work for ROR with 100 bonds and progressive params', () => {
+  it('should work for ROR with 1000 bonds and progressive params', () => {
     const calculationParams: CalculationParams = {
       inflation: [
         0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.11, 0.12,
@@ -210,9 +117,9 @@ describe('Calculate treasury bonds', () => {
         0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.11, 0.12,
       ],
     };
-    const result = calculate(ROR, 100, 144, calculationParams);
+    const result = calculate(ROR, 1000, 144, calculationParams);
     const expectedResult144: Partial<TreasuryBondsMonthlyResult> = {
-      finalValueAtEndOfMath: 18859.09,
+      finalValueAtEndOfMath: 197714.48,
     };
     deepStrictEqual(result[144], { ...result[144], ...expectedResult144 });
   });
--- functions/src/treasury-bonds/treasury-bonds.service.ts
+++ functions/src/treasury-bonds/treasury-bonds.service.ts
@@ -14,8 +14,17 @@ const TAX = 0.19;
 const BOND_COST = 100;
 const MAX_MONTH = 144;
 
-export function calculate(treasuryBonds: TreasuryBonds, initialNumberOfBonds: number, maxMonth: number, calculationParams: CalculationParams): TreasuryBondsResult {
-  const calculateForMonth = calculateForMonthFactory(treasuryBonds, initialNumberOfBonds, calculationParams);
+export function calculate(
+  treasuryBonds: TreasuryBonds,
+  initialNumberOfBonds: number,
+  maxMonth: number,
+  calculationParams: CalculationParams
+): TreasuryBondsResult {
+  const calculateForMonth = calculateForMonthFactory(
+    treasuryBonds,
+    initialNumberOfBonds,
+    calculationParams
+  );
 
   const months = [...Array(maxMonth).keys()].map((i) => i + 1);
   const result = months.reduce<TreasuryBondsResult>(
@@ -23,13 +32,15 @@ export function calculate(treasuryBonds: TreasuryBonds, initialNumberOfBonds: nu
       ...acc,
       [month]: calculateForMonth(month, acc[month - 1]),
     }),
-    {},
+    {}
   );
 
   return normalizeResult(result);
 }
 
-export function simplifyResult(result: TreasuryBondsResult): TreasuryBondsSimpleResult {
+export function simplifyResult(
+  result: TreasuryBondsResult
+): TreasuryBondsSimpleResult {
   return mapValues(result, (value) => value.finalValueAtEndOfMath);
 }
 
@@ -55,9 +66,13 @@ function normalizeResult(result: TreasuryBondsResult): TreasuryBondsResult {
 function calculateForMonthFactory(
   treasuryBonds: TreasuryBonds,
   initialNumberOfBonds: number,
-  calculationParams: CalculationParams,
-): (month: number, previousMonthResult: TreasuryBondsMonthlyResult) => TreasuryBondsMonthlyResult {
-  const calculateParamsByMonth = createCalculationParamsByMonthFn(calculationParams);
+  calculationParams: CalculationParams
+): (
+  month: number,
+  previousMonthResult: TreasuryBondsMonthlyResult
+) => TreasuryBondsMonthlyResult {
+  const calculateParamsByMonth =
+    createCalculationParamsByMonthFn(calculationParams);
 
   return (month, previousMonthResult) => {
     const currentCalculationParams = calculateParamsByMonth(month);
@@ -67,24 +82,71 @@ function calculateForMonthFactory(
     const amount = initialNumberOfBonds * BOND_COST;
 
     const maturity = month % treasuryBonds.maturityInMonths === 0;
-    const numberOfBonds = previousMonthResult?.nextNumberOfBonds || initialNumberOfBonds;
+    const numberOfBonds =
+      previousMonthResult?.nextNumberOfBonds || initialNumberOfBonds;
     const buyCost = previousMonthResult?.nextBuyCost || amount;
     const nominalValue = previousMonthResult?.nextNominalValue || amount;
-    const basisOfCapitalisation = previousMonthResult?.nextBasisOfCapitalisation || nominalValue;
+    const basisOfCapitalisation =
+      previousMonthResult?.nextBasisOfCapitalisation || nominalValue;
     const rate = calculateRate(maturity, indexation)(treasuryBonds, month);
-    const valueAtEndOfMonth = calculateValueAtEndOfMonth(basisOfCapitalisation, rate)(treasuryBonds, month);
-    const earlyBuyoutPenalty = calculateEarlyBuyoutPenalty(nominalValue, maturity, numberOfBonds, valueAtEndOfMonth)(treasuryBonds, month);
-    const buyoutAtEndOfMonth = calculateBuyoutAtEndOfMonth(nominalValue, valueAtEndOfMonth, earlyBuyoutPenalty);
-
-    const nextNumberOfBonds = calculateNextNumberOfBonds(numberOfBonds, maturity, buyoutAtEndOfMonth)(treasuryBonds);
-    const nextBuyCost = calculateNextBuyCost(maturity, nextNumberOfBonds, buyCost)(treasuryBonds);
-    const nextNominalValue = calculateNextNominalValue(nextNumberOfBonds, maturity, nominalValue);
-    const nextBasisOfCapitalisation = calculateNextBasisOfCapitalisation(treasuryBonds, month, maturity, nextNominalValue, basisOfCapitalisation, valueAtEndOfMonth);
-
-    const profitAtEndOfMonth = calculateProfitAtEndOfMonth(maturity, valueAtEndOfMonth, nominalValue, nextBuyCost, buyoutAtEndOfMonth)(treasuryBonds, month);
+    const valueAtEndOfMonth = calculateValueAtEndOfMonth(
+      basisOfCapitalisation,
+      rate
+    )(treasuryBonds, month);
+    const earlyBuyoutPenalty = calculateEarlyBuyoutPenalty(
+      nominalValue,
+      maturity,
+      numberOfBonds,
+      valueAtEndOfMonth
+    )(treasuryBonds, month);
+    const buyoutAtEndOfMonth = calculateBuyoutAtEndOfMonth(
+      nominalValue,
+      valueAtEndOfMonth,
+      earlyBuyoutPenalty
+    );
+    const profitAtEndOfMonth = calculateProfitAtEndOfMonth(
+      maturity,
+      valueAtEndOfMonth,
+      nominalValue
+    )(treasuryBonds, month);
     const previousBalance = previousMonthResult?.balanceAtEndOfMonth || 0;
-    const balanceAtEndOfMonth = calculateBalanceAtEndOfMonth(previousBalance, savingsRate, profitAtEndOfMonth);
-    const finalValueAtEndOfMath = previousBalance * (1 + (savingsRate / 12) * (1 - TAX)) + buyoutAtEndOfMonth;
+    const previousMaturity = previousMonthResult?.maturity || false;
+    const balanceAtEndOfMonth = calculateBalanceAtEndOfMonth(
+      previousMaturity,
+      previousBalance,
+      savingsRate,
+      profitAtEndOfMonth
+    );
+
+    const nextNumberOfBonds = calculateNextNumberOfBonds(
+      numberOfBonds,
+      maturity,
+      buyoutAtEndOfMonth,
+      balanceAtEndOfMonth
+    )(treasuryBonds);
+    const nextBuyCost = calculateNextBuyCost(
+      maturity,
+      buyCost,
+      buyoutAtEndOfMonth,
+      balanceAtEndOfMonth
+    )(treasuryBonds);
+    const nextNominalValue = calculateNextNominalValue(
+      nextNumberOfBonds,
+      maturity,
+      nominalValue
+    );
+    const nextBasisOfCapitalisation = calculateNextBasisOfCapitalisation(
+      treasuryBonds,
+      month,
+      maturity,
+      nextNominalValue,
+      basisOfCapitalisation,
+      valueAtEndOfMonth
+    );
+
+    const finalValueAtEndOfMath =
+      previousBalance * (1 + (savingsRate / 12) * (1 - TAX)) +
+      buyoutAtEndOfMonth;
 
     return {
       numberOfBonds,
@@ -107,70 +169,173 @@ function calculateForMonthFactory(
   };
 }
 
-function calculateRate(maturity: boolean, indexation: number): CalculateByTreasuryBonds {
+function calculateRate(
+  maturity: boolean,
+  indexation: number
+): CalculateByTreasuryBonds {
   return (treasuryBonds, month) => {
-    const isFirstPeriod = month! % treasuryBonds.maturityInMonths <= treasuryBonds.rateUpdateInMonths && !maturity;
-    return isFirstPeriod ? treasuryBonds.firstPeriodRate : treasuryBonds.margin + indexation;
+    const isFirstPeriod =
+      month! % treasuryBonds.maturityInMonths <=
+        treasuryBonds.rateUpdateInMonths && !maturity;
+    return isFirstPeriod
+      ? treasuryBonds.firstPeriodRate
+      : treasuryBonds.margin + indexation;
   };
 }
 
-function calculateValueAtEndOfMonth(amount: number, rate: number): CalculateByTreasuryBonds {
+function calculateValueAtEndOfMonth(
+  amount: number,
+  rate: number
+): CalculateByTreasuryBonds {
   return (treasuryBonds, month) => {
-    const interestCalculationInMonths = treasuryBonds.paymentOfInterestInMonths || treasuryBonds.capitalizationOfInterestInMonths;
-    const paymentOfInterest = (month! % interestCalculationInMonths || interestCalculationInMonths) / 12;
+    const interestCalculationInMonths =
+      treasuryBonds.paymentOfInterestInMonths ||
+      treasuryBonds.capitalizationOfInterestInMonths;
+    const paymentOfInterest =
+      (month! % interestCalculationInMonths || interestCalculationInMonths) /
+      12;
     return amount * (1 + rate * paymentOfInterest);
   };
 }
 
-function calculateEarlyBuyoutPenalty(nominalValue: number, maturity: boolean, numberOfBonds: number, valueAtEndOfMonth: number): CalculateByTreasuryBonds {
+function calculateEarlyBuyoutPenalty(
+  nominalValue: number,
+  maturity: boolean,
+  numberOfBonds: number,
+  valueAtEndOfMonth: number
+): CalculateByTreasuryBonds {
   return (treasuryBonds, month) => {
     const profit = valueAtEndOfMonth - nominalValue;
-    const protection = month! % treasuryBonds.maturityInMonths <= treasuryBonds.buyoutProtectionInMonths;
+    const protection =
+      month! % treasuryBonds.maturityInMonths <=
+      treasuryBonds.buyoutProtectionInMonths;
     const penalty = numberOfBonds * treasuryBonds.earlyBuyoutCost;
     return maturity ? 0 : protection ? Math.min(profit, penalty) : penalty;
   };
 }
 
-function calculateBuyoutAtEndOfMonth(nominalValue: number, valueAtEndOfMonth: number, earlyBuyoutPenalty: number): number {
-  return valueAtEndOfMonth - earlyBuyoutPenalty - (valueAtEndOfMonth - earlyBuyoutPenalty - nominalValue) * TAX;
+function calculateBuyoutAtEndOfMonth(
+  nominalValue: number,
+  valueAtEndOfMonth: number,
+  earlyBuyoutPenalty: number
+): number {
+  return (
+    valueAtEndOfMonth -
+    earlyBuyoutPenalty -
+    (valueAtEndOfMonth - earlyBuyoutPenalty - nominalValue) * TAX
+  );
 }
 
-function calculateNextBuyCost(maturity: boolean, nextNumberOfBonds: number, buyCost: number): CalculateByTreasuryBonds {
-  return (treasuryBonds) => (maturity ? nextNumberOfBonds * treasuryBonds.exchangeCost : buyCost);
-}
-
-function calculateNextNumberOfBonds(numberOfBonds: number, maturity: boolean, buyoutAtEndOfMonth: number): CalculateByTreasuryBonds {
+function calculateNextBuyCost(
+  maturity: boolean,
+  buyCost: number,
+  buyoutAtEndOfMonth: number,
+  balanceAtEndOfMonth: number
+): CalculateByTreasuryBonds {
   return (treasuryBonds) => {
-    const whenMaturity = Math.floor(buyoutAtEndOfMonth / treasuryBonds.exchangeCost);
-    return maturity ? whenMaturity : numberOfBonds;
+    if (!maturity) {
+      return buyCost;
+    }
+
+    return (
+      Math.floor(buyoutAtEndOfMonth / treasuryBonds.exchangeCost) *
+        treasuryBonds.exchangeCost +
+      Math.floor(balanceAtEndOfMonth / 100) * 100
+    );
   };
 }
 
-function calculateNextNominalValue(nextNumberOfBonds: number, maturity: boolean, nominalValue: number): number {
+function calculateNextNumberOfBonds(
+  numberOfBonds: number,
+  maturity: boolean,
+  buyoutAtEndOfMonth: number,
+  balanceAtEndOfMonth: number
+): CalculateByTreasuryBonds {
+  return (treasuryBonds) => {
+    if (!maturity) {
+      return numberOfBonds;
+    }
+
+    return (
+      Math.floor(buyoutAtEndOfMonth / treasuryBonds.exchangeCost) +
+      Math.floor(balanceAtEndOfMonth / 100)
+    );
+  };
+}
+
+function calculateNextNominalValue(
+  nextNumberOfBonds: number,
+  maturity: boolean,
+  nominalValue: number
+): number {
   return maturity ? nextNumberOfBonds * BOND_COST : nominalValue;
 }
 
-function calculateProfitAtEndOfMonth(maturity: boolean, valueAtEndOfMonth: number, nominalValue: number, nextBuyCost: number, buyoutAtEndOfMonth: number): CalculateByTreasuryBonds {
+function calculateProfitAtEndOfMonth(
+  maturity: boolean,
+  valueAtEndOfMonth: number,
+  nominalValue: number
+): CalculateByTreasuryBonds {
   return (treasuryBonds, month) => {
     if (month! >= MAX_MONTH) {
       return 0;
     }
 
-    const isPaymentOfInterest = treasuryBonds.paymentOfInterestInMonths;
+    //     =IF(MOD($AA55;wyplata_odsetek_ROR)=0; (AJ55-AG55)*(1-podatek_Belki);0)
+    // +IF(AK55="tak";ROUNDDOWN(AJ55/zamiana_ROR;0)*(100-zamiana_ROR);0)
+    // aa55 - miesiac
+    // wyplata_odsetek_ROR - co ile miesiecy wyplata odsetek
+    // AJ55 - wartosc na koniec miesiaca
+    // AG55 - nominalna wartosc
 
     const profitIfNoPayment = (valueAtEndOfMonth - nominalValue) * (1 - TAX);
-    const profit = month! % treasuryBonds.paymentOfInterestInMonths ? 0 : profitIfNoPayment;
-    const nextCost = maturity && nextBuyCost ? nextBuyCost - nominalValue : 0;
-    const withPaymentOfInterest = profit - nextCost;
+    const profit =
+      month! % treasuryBonds.paymentOfInterestInMonths ? 0 : profitIfNoPayment;
+    // jak to nazwac?
+    const test = maturity
+      ? Math.floor(valueAtEndOfMonth / treasuryBonds.exchangeCost) *
+        (100 - treasuryBonds.exchangeCost)
+      : 0;
 
-    const withoutPaymentOfInterest = maturity ? buyoutAtEndOfMonth - nextBuyCost : 0;
+    return profit + test;
 
-    return isPaymentOfInterest ? withPaymentOfInterest : withoutPaymentOfInterest;
+    // const isPaymentOfInterest = treasuryBonds.paymentOfInterestInMonths;
+
+    // const profitIfNoPayment = (valueAtEndOfMonth - nominalValue) * (1 - TAX);
+    // const profit =
+    //   month! % treasuryBonds.paymentOfInterestInMonths ? 0 : profitIfNoPayment;
+    // const nextCost = maturity && nextBuyCost ? nextBuyCost - nominalValue : 0;
+    // const withPaymentOfInterest = profit - nextCost;
+
+    // const withoutPaymentOfInterest = maturity
+    //   ? buyoutAtEndOfMonth - nextBuyCost
+    //   : 0;
+
+    // return isPaymentOfInterest
+    //   ? withPaymentOfInterest
+    //   : withoutPaymentOfInterest;
   };
 }
 
-function calculateBalanceAtEndOfMonth(previousBalance: number, savingsRate: number, profitAtEndOfMonth: number): number {
-  return previousBalance * (1 + (savingsRate / 12) * (1 - TAX)) + profitAtEndOfMonth;
+function calculateBalanceAtEndOfMonth(
+  previousMaturity: boolean,
+  previousBalance: number,
+  savingsRate: number,
+  profitAtEndOfMonth: number
+): number {
+  //   =(AP54-IF(AK54="tak";ROUNDDOWN(AP54/100;0)*100;0))*
+  // (1+AO55/12*(1-podatek_Belki))+AN55
+  // AP54 - poprzedni balance
+  // AK54 - czy jest zapadalnosc
+  // AO55 - stopa oprocentowania
+  // AN55 - zysk na koniec miesiaca
+
+  return (
+    (previousBalance -
+      (previousMaturity ? Math.floor(previousBalance / 100) * 100 : 0)) *
+      (1 + (savingsRate / 12) * (1 - TAX)) +
+    profitAtEndOfMonth
+  );
 }
 
 function calculateNextBasisOfCapitalisation(
@@ -179,16 +344,22 @@ function calculateNextBasisOfCapitalisation(
   maturity: boolean,
   nextNominalValue: number,
   basisOfCapitalisation: number,
-  valueAtEndOfMonth: number,
+  valueAtEndOfMonth: number
 ): number {
   if (!treasuryBonds.capitalizationOfInterestInMonths) {
     return nextNominalValue;
   }
 
-  return maturity ? nextNominalValue : (month + 1) % treasuryBonds.capitalizationOfInterestInMonths !== 1 ? basisOfCapitalisation : valueAtEndOfMonth;
+  return maturity
+    ? nextNominalValue
+    : (month + 1) % treasuryBonds.capitalizationOfInterestInMonths !== 1
+    ? basisOfCapitalisation
+    : valueAtEndOfMonth;
 }
 
-export function createCalculationParamsByMonthFn(params: CalculationParams): CalculationParamsByMonthFn {
+export function createCalculationParamsByMonthFn(
+  params: CalculationParams
+): CalculationParamsByMonthFn {
   return (month) => {
     const year = Math.ceil(month / 12);
     return {
